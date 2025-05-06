18690948bdb5cdba56525fe310c5f52a0a1ad433
Ran Philosof <ranphilosof@gmail.com>
2025-05-06 07:36:52 +0300

    Reference Cs Proj On Debug & Nugets On Release

--- Temp_TablePub_Sampler_Comm/RestTestApp/ProbeExample.cs
+++ Temp_TablePub_Sampler_Comm/RestTestApp/ProbeExample.cs
@@ -1,4 +1,5 @@
-﻿using RP.Prober.Singleton;
+﻿using RP.Prober.CyclicCacheProbing;
+using RP.Prober.Singleton;
 
 namespace RestTestApp
 {
@@ -72,6 +73,65 @@ namespace RestTestApp
         private static string GetRandomWord()
         {
             var words = new List<string> { "apple", "banana", "cherry", "dog", "elephant", "flower" };
+            var random = new Random();
+            int index = random.Next(words.Count);
+            return words[index];
+        }
+    }
+
+    public class SamplesCreator
+    {
+        private KeyedCacheProbing<string, Measurment> keyedCacheProbing;
+
+        public SamplesCreator(string name)
+        {
+            keyedCacheProbing =
+                new KeyedCacheProbing<string, Measurment>(name, (new List<string>() { "Key" }.Concat(Measurment.Headers).ToList()), true);
+
+            keyedCacheProbing.Convert =
+                (key, measure) =>
+                {
+                    return new List<string>()
+                            {
+                                $"{key}",
+                                $"{measure.Counter}",
+                                $"{measure.Height}",
+                                $"{measure.Width}",
+                                $"{measure.Size}",
+                                $"{measure.Description}"
+                            };
+                };
+        }
+
+        public void StartGenerateLines()
+        {
+            Task.Run(
+                () =>
+                {
+                    while (true)
+                    {
+                        Thread.Sleep(100);
+                        GenerateNewLine();
+                    }
+                });
+        }
+
+        private void GenerateNewLine()
+        {
+            keyedCacheProbing.SetKeyValue(GetRandomKey(), new Measurment(Random.Shared.Next(1, 10), Random.Shared.Next(11, 20), Random.Shared.Next(21, 30), GetRandomWord()));
+        }
+
+        private static string GetRandomWord()
+        {
+            var words = new List<string> { "apple", "banana", "cherry", "dog", "elephant", "flower" };
+            var random = new Random();
+            int index = random.Next(words.Count);
+            return words[index];
+        }
+
+        private static string GetRandomKey()
+        {
+            var words = new List<string> { "Flow1", "Flow2", "Flow3", "Flow4", "Flow5", "Flow6" };
             var random = new Random();
             int index = random.Next(words.Count);
             return words[index];
--- Temp_TablePub_Sampler_Comm/RestTestApp/Program.cs
+++ Temp_TablePub_Sampler_Comm/RestTestApp/Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using RestTestApp;
+using RP.Prober.CyclicCacheProbing;
 using System.Threading.Tasks;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -34,11 +35,14 @@ app.UseAuthorization();
 app.MapControllers();
 
 
-var measurmentsCreator = new MeasurmentsCreator("Demo1", RP.Prober.Singleton.HeaderType.Row, 1);
+var measurmentsCreator = new MeasurmentsCreator("Demo1", HeaderType.Row, 1);
 measurmentsCreator.StartGenerateLines();
 
-var measurmentsCreator2 = new MeasurmentsCreator("Demo2", RP.Prober.Singleton.HeaderType.Column, 7);
+var measurmentsCreator2 = new MeasurmentsCreator("Demo2", HeaderType.Column, 7);
 measurmentsCreator2.StartGenerateLines();
+
+var samplesCreator = new SamplesCreator("Demo3");
+samplesCreator.StartGenerateLines();
 
 var tt = new TT();
 Task.Run(() => tt.PubSub_N_Records());
